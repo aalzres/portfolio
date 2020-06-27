559733fb33ce7a168835aee3095daceb17ae9537
Andrés Felipe Alzate Restrepo <aalzres@gmail.com>
2020-06-27 17:51:11 +0200

    Implement check email

--- PORTFOLIO/Code/Utils/Presentation/Extension/String+Extension.swift
+++ PORTFOLIO/Code/Utils/Presentation/Extension/String+Extension.swift
@@ -9,6 +9,13 @@
 import Foundation
 
 extension String {
+    var isValidEmail: Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self)
+    }
+    
     func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
         return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
     }
