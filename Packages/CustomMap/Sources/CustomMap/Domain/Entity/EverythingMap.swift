f086b1f0db609b821d75cf461a7091bceada52d1
Andrés Felipe <aalzres@gmail.com>
2022-10-30 20:33:16 +0000

    Entity implemented  for the map

---
+++ Packages/CustomMap/Sources/CustomMap/Domain/Entity/EverythingMap.swift
@@ -0,0 +1,10 @@
+//
+//  EverythingMap.swift
+//  
+//
+//  Created by Andres Felipe Alzate Restrepo on 30/10/22.
+//
+
+public protocol EverythingMap {
+    var mapElements: [MapElement] { get }
+}
